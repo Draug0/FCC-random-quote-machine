6d08ddabc91c273d43a439ed6c1a27a134e0f02a
Draug0 <85965310+Draug0@users.noreply.github.com>
2021-11-09 18:59:30 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,33 @@
+let quotesArr;
+
+function fetchQuotes() {
+    return $.ajax({
+        url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+        success: function(data) {
+            quotesArr = JSON.parse(data)
+            //console.log(quotes) 
+        }
+    })
+}
+
+function randomQuote() {
+    return quotesArr.quotes[Math.floor(Math.random() * quotesArr.quotes.length)];
+}
+
+
+ $(document).ready(() => {
+    fetchQuotes().then(() => {
+        let random = randomQuote()
+        $('#text').text(random.quote)
+        $('#author').text(random.author)
+    })
+
+    $('#new-quote').click(() => {
+        fetchQuotes().then(() => {
+            let random = randomQuote()
+            $('#text').text(random.quote)
+            $('#author').text(random.author)
+        })
+    })
+});
+
